48710c942acc623406eb41366359a874c5f95b50
Harishankar <94908935+MrKolaruGaming@users.noreply.github.com>
2023-10-10 18:33:26 +0530

    api intergration

--- talentship.js
+++ talentship.js
@@ -1,61 +1,59 @@
-let UserData = [
-    {
-        "userId": 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 1,
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false
-    },
-    {
-        "userId": 1,
-        "id": 4,
-        "title": "et porro tempora",
-        "completed": true
-    },
-    {
-        "userId": 1,
-        "id": 5,
-        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-        "completed": false
-    },
-    {
-        "userId": 1,
-        "id": 6,
-        "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-        "completed": false
-    },
-    {
-        "userId": 1,
-        "id": 6,
-        "title": "Harishankar",
-        "completed": false
-    }
-];
+  
+var UserData ;
 
+ fetch('https://jsonplaceholder.typicode.com/todos').then((res) => res.json())
+    .then((UserData) =>{
+ console.log(UserData);
+ createTable(UserData);
+})
 
 function createTable(UserData) {
-    let table = document.getElementById("DataTable").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("DataTable").getElementsByTagName('tbody')[0];
 
-    for (let i = 0; i < UserData.length; i++) {
-        let row = table.insertRow(i);
+    for (var i = 0; i < UserData.length; i++) {
+        var row = table.insertRow(i);
 
-        let userIdCell = row.insertCell(0);
+        var userIdCell = row.insertCell(0);
         userIdCell.innerHTML = UserData[i].userId;
 
-        let idCell = row.insertCell(1);
+        var idCell = row.insertCell(1);
         idCell.innerHTML = UserData[i].id;
 
-        let titleCell = row.insertCell(2);
+        var titleCell = row.insertCell(2);
         titleCell.innerHTML = UserData[i].title;
 
-        let completedCell = row.insertCell(3);
+        var completedCell = row.insertCell(3);
         completedCell.innerHTML = UserData[i].completed ? "True" : "False";
     }
 }
 
-createTable(UserData);
+
+
+ function filtertable(){
+     var input, filter, table , tr, td, i, j, textValue;
+     input = document.getElementById('InputFilter');
+     filter = input.value.toUpperCase();
+     table = document.getElementById('DataTable');
+     tr = document.getElementsByTagName('tr');
+
+     for (let i = 0; i < tr.length; i++) {
+         tr[i].style.display = "none" ;
+
+         td = tr[i].getElementsByTagName('td');
+
+     for (let j = 0; j < td.length; j++) {
+        textValue = td[j].textContent || td[j].innerText;
+
+        if (textValue.toUpperCase().indexOf(filter) > -1){
+            tr[i].style.display = "" ;
+           break;
+           }
+        
+     }
+        
+     }
+ }
+
+ createTable (UserData);
+
+document.getElementById("InputFilter").addEventListener("keyup", filtertable);
